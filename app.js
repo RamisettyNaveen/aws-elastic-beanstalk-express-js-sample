c9e84c9d1a97a027e1e6823c30da5032a5414096
RamisettyNaveen <137636617+RamisettyNaveen@users.noreply.github.com>
2023-09-01 20:09:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to K21 Academy'));
+app.get('/', (req, res) => res.send('we have added a review stage with manual approval to our cont'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
